742d03d68ecd0b9c0013e4204c09d276cf2e1824
PES1UG21CS126 <PES1202100527@pesu.pes.edu>
2024-03-09 16:09:10 +0530

    modified file

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "I am PES1UG21CS126\n";
+  cout << "I AM making changes\n";
+  cout << "PES1UG21CS126\n";
   return 0;
 }
